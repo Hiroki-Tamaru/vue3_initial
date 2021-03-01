4137349779f82fc7e65c6a40d9dd9ed13b4634d9
Hiroki-Tamaru <76980504+Hiroki-Tamaru@users.noreply.github.com>
2021-03-01 11:59:57 +0900

    service worker導入
    以下のサイトを参考に、service workerを導入し、キャッシュを追加・読み込みできるようにした。
    https://dev.to/voodu/vue-3-pwa-service-worker-12di
    ただし、このサイトでは古いworkbox-expirationを使っているようで動かなかったため、関連部分をコメントアウトした。

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// service worker
+import runtime from "serviceworker-webpack-plugin/lib/runtime";
+
+if ("serviceWorker" in navigator) {
+  runtime.register();
+}
+
 createApp(App).mount('#app')
